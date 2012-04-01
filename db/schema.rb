c8a8be9a0306ddb7a956d452dcc76b1cff3a3358
Laurence Guild <jrubiando@gmail.com>
2012-04-01 10:40:27 -0400

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120401025210) do
+ActiveRecord::Schema.define(:version => 20120401130642) do
 
   create_table "planes", :force => true do |t|
     t.float    "longitude"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20120401025210) do
     t.float    "latitude_movement"
     t.float    "longitude_movement"
     t.integer  "speed"
+    t.string   "plane_type"
   end
 
 end
