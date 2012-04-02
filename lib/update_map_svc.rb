4548e12e89f1255e5097495a0e5b8ede7005b37f
Laurence Guild <jrubiando@gmail.com>
2012-04-02 00:34:44 -0400

    port can be passed as argument

--- lib/update_map_svc.rb
+++ lib/update_map_svc.rb
@@ -13,20 +13,26 @@ class Server < EventMachine::Connection
   def receive_data(message)
     # create a Faye client
     client = Faye::Client.new('http://localhost:9292/faye')
-    
-    p message
-    
+       
     # publish the flight info
     client.publish('/messages/public', 'json' => message)                  
-    puts "#{@status} -- #{message}"        
+    # puts "#{@status} -- #{message}"        
   end
 end
 
 # create a service through event machine to read messages through port 2000
 # and send them to Faye.
 
+# default port is 2000, 
+# but another port can be specified on the command line
+port = 2000
+if ARGV[0]
+  port = ARGV[0].to_i
+end
+
+
 EventMachine.run do
-  EventMachine.start_server 'localhost', 2000, Server do |conn|
+  EventMachine.start_server 'localhost', port, Server do |conn|
     conn.options = {:type => 'faye_service'}
     conn.status = :OK
   end
